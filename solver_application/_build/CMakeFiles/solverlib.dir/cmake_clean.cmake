bf339cf81b68049cdb8ab572c7c40e51f9c553bf
SergeyMorrison <99497212+SergeyMorrison@users.noreply.github.com>
2022-04-07 18:01:51 +0300

    Add files via upload

---
+++ solver_application/_build/CMakeFiles/solverlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solverlib.dir/home/morrison/TIMP03/solver_lib/solver.cpp.o"
+  "libsolverlib.a"
+  "libsolverlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solverlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ solver_application/_build/CMakeFiles/solverlib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libsolverlib.a"
+)
